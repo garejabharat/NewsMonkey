77255efa2102b5d257cecdabed945b5db022f44a
Bharat <garejabharatk@gmail.com>
2023-03-23 09:47:04 +0530

    create comp file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import React, { Component } from 'react'
 import Navbar from './component/Navbar';
+import Comp from './component/Comp';
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
       <>
       
       <Navbar/>
+      <Comp />
       </>
     )
   }
---
+++ src/component/Comp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Comp extends Component {
+  render() {
+    return (
+      <div>this is git simple text</div>
+    )
+  }
+}
