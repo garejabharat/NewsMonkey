967221c5faef6cb9b9e068db606b03909657dacf
Bharat <garejabharatk@gmail.com>
2023-03-23 09:32:34 +0530

    add news app in git

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+import Navbar from './component/Navbar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      
+      <Navbar/>
+      </>
+    )
+  }
+}
+
+
