7c34a908bdf009afc5b18085502c930f4fc27994
Bharat <garejabharatk@gmail.com>
2023-05-02 09:36:10 +0530

    Updates

---
+++ precache-manifest.5ab40aa9fad81d29e7e3cc741e9bf590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8d72001755749c32cc9",
+    "url": "/static/css/main.24e709ee.chunk.css"
+  },
+  {
+    "revision": "a8d72001755749c32cc9",
+    "url": "/static/js/main.a8d72001.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "3712281bd00e72b84ba4",
+    "url": "/static/js/2.3712281b.chunk.js"
+  },
+  {
+    "revision": "c13a65875c5c3d089b96",
+    "url": "/static/js/runtime~main.c13a6587.js"
+  },
+  {
+    "revision": "cf152bdd591a5f5d5fe951536f0a858f",
+    "url": "/static/media/loading.cf152bdd.gif"
+  },
+  {
+    "revision": "e8437789586ce012083b7fccb7a7d070",
+    "url": "/index.html"
+  }
+];
